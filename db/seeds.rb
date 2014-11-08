16ea6de9f45536afe38db67a47b0c2fb36cfcfa9
Szymon Borucki <szymon.borucki@gmail.com>
2014-11-08 15:40:03 -0500

    Added seed with dummy data and Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+category = ["Gyms", "School", "Drug store", "Sex", "Family"]
+category.each do |cat|
+  Category.create(name: cat)
+end
+
+tags = ["Humber Only", "Seneca Only", "Repeats Monthly", "Addiction"]
+tags.each do |tag|
+  Tag.create(name: tag)
+end
+
+10.times do |poi|
+  Poi.create(name: Faker::Company.name, address: Faker::Address.street_address, description: Faker::Lorem.sentence, category_id: rand(1..5))
+end
