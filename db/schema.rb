91dea0b93185241618e5dfeed54a2a940adb43ab
Szymon Borucki <szymon.borucki@gmail.com>
2014-11-08 12:17:19 -0500

    Added tags and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108170806) do
+ActiveRecord::Schema.define(version: 20141108171629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pois", force: true do |t|
     t.string   "name"
@@ -23,6 +29,12 @@ ActiveRecord::Schema.define(version: 20141108170806) do
     t.float    "latitude"
     t.float    "longitude"
     t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
